b39013215a140b96b52fc83338aaa99c352dc9ca
mtiahrt <mark.tiahrt@outlook.com>
2021-12-13 15:01:46 -0700

    added icon on meal detail

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
-
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { MEALS } from '../data/dummy-data';
+import CustomHeaderButton from '../components/HeaderButton';
 
 const MealDetailScreen = props => {
   const mealId = props.navigation.getParam('mealId');
@@ -25,7 +26,12 @@ MealDetailScreen.navigationOptions = navigationData => {
   const mealId = navigationData.navigation.getParam('mealId');
   const selectedMeal = MEALS.find(meal => meal.id === mealId);
   return {
-    headerTitle: selectedMeal.title
+    headerTitle: selectedMeal.title,
+    headerRight: <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+      <Item title='Favorite' iconName='ios-star' onPress={() => {
+        console.log('mark as your favorate')
+      }}/>
+    </HeaderButtons>
   };
 };
 
